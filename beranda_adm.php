f9c95c0ac4b2922bee63140b2c5d20fdba80993b
dewiaulia47437 <98312205+dewiaulia47437@users.noreply.github.com>
2022-01-24 16:09:23 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Sekolah</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/sekolah.png" width="200px"></img></p>
+                          <p class="label-title" align="center"><strong>Sistem Informasi Sekolah</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="justify">
+                            Sistem Informasi Sekolah adalah sebuah sistem informasi manajemen sekolah berbasis website sebagai solusi administrasi terpadu untuk sekolah, yang memiliki beberapa modul terintegrasi yang dapat diakses oleh semua anggota sekolah (guru, wali kelas, pegawai sekolah dan siswa) dan orang tua/wali siswa.
+
+Sim Sekolah membantu sekolah untuk mengakomodir kegiatan administrasi internal sekolah mencakup kebutuhan manajemen sekolah dan merekatkan komunikasi antara pihak sekolah dengan siswa maupun orang tua/wali siswa.
+
+Dengan teknologi informasi dan memanfaatkan perkembangannya, manajemen sekolah dapat diselenggarakan secara lebih efektif, efisien, akuntabel dan transparan. Sim Sekolah dikembangkan berdasarkan kebutuhan sekolah, standarisasi diknas (dapodik) dan perkembangan teknologi informasi yang sangat cepat.
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            Copyright &COPY; <?= date('Y')?> Sistem Informasi Sekolah |
+            <a href="#">SI 7P</a>
+        </div>
+    </div>
+</div>
